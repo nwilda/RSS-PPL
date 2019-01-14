2fcf6022661181529e606c1481150f3ae582c2c5
nwilda <nwildaas@gmail.com>
2019-01-14 16:33:32 +0700

    initial rss upload

---
+++ RssRedaer/app/src/main/java/com/loopwiki/rssredaer/MainActivity.java
@@ -0,0 +1,26 @@
+package com.loopwiki.rssredaer;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerview);
+        //Call Read rss asyntask to fetch rss
+        ReadRss readRss = new ReadRss(this, recyclerView);
+        readRss.execute();
+    }
+
+}
