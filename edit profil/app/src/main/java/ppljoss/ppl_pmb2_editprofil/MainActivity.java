43f792a0082f3eb21e4f3e29aea9d8c9efb6b46d
nwilda <nwildaas@gmail.com>
2019-01-31 23:26:01 +0700

    layout button

---
+++ edit profil/app/src/main/java/ppljoss/ppl_pmb2_editprofil/MainActivity.java
@@ -0,0 +1,13 @@
+package ppljoss.ppl_pmb2_editprofil;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
